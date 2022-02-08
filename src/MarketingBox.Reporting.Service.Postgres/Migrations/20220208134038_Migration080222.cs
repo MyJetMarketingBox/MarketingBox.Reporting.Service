1a23b47de1941c9bf38d5c350bf050a3e1cc8c54
Alexey <alexey.p@smplt.net>
2022-02-08 16:40:48 +0300

    update models

---
+++ src/MarketingBox.Reporting.Service.Postgres/Migrations/20220208134038_Migration080222.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketingBox.Reporting.Service.Postgres.Migrations
+{
+    public partial class Migration080222 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DepositDate",
+                schema: "reporting-service",
+                table: "registrations_details",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepositDate",
+                schema: "reporting-service",
+                table: "registrations_details");
+        }
+    }
+}
